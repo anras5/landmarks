15f47f59a88c71f7a8e640ef00dfc9b55372fd72
anras5 <anras1filip@gmail.com>
2024-12-22 11:46:10 +0100

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Filip Marciniak on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
